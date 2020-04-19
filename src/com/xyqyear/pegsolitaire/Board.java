bda611c01f8058514543055d0707939914e9577b
TracerChen <TracerChen0624@gmail.com>
2020-04-19 16:19:14 +0800

    board of the game

--- src/com/xyqyear/pegsolitaire/Board.java
+++ src/com/xyqyear/pegsolitaire/Board.java
@@ -13,18 +13,29 @@ public class Board {
     private int pieceNum = 32;
 
     public State getPiece(int x, int y) {
+    	
         return board[y][x];
     }
 
     // 设置某一个位置的棋子为给定状态
     // 注意： 要修改pieceNum， 比如这个位置本来有棋子，后来没棋子，pieceNum就减一
     public void setPiece(int x, int y, State state) {
+    	if (board[y][x] == State.EXIST && state == State.TAKEN)
+    		this.pieceNum--;
+    	else if(board[y][x] == State.TAKEN && state == State.EXIST)
+    		this.pieceNum++;
+        this.board[y][x] = state;
     }
 
     // 将参数的棋盘复制到自己里面，包含棋子和pieceNum
     // 注意： 数组的复制要用for循环深拷贝
     public void update(Board board) {
-
+    	for(int i = 0; i < 7; i++) {
+    		for(int j = 0; j < 7; j++) {
+    			this.board[j][i] = board.getPiece(i,j);
+    		}
+    	}
+    	 this.pieceNum = board.getPieceNum();
     }
 
     // 获取棋子的个数
