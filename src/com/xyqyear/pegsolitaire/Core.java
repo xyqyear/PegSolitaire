326ac2d7d747a5623539e8de577792cbb4826173
xyqyear <215908558@qq.com>
2020-04-19 16:16:18 +0800

    added comments to Board

--- src/com/xyqyear/pegsolitaire/Core.java
+++ src/com/xyqyear/pegsolitaire/Core.java
@@ -12,18 +12,27 @@ public class Core {
         boardHistory.clear();
     }
 
+    // 判断这样子走棋是否非法
     public boolean canDoStep(int fromX, int fromY, int toX, int toY) {
         return false;
     }
 
+    // 实质上走棋
+    // 先用canDoStep方法考虑是否可以走，如果不能走直接返回false
+    // 如果可以走那么就先在历史棋盘里添加一个board对象，把当前的棋盘复制进去，方便悔棋
+    // 然后再利用孔明棋的规则走棋
+    // 注: 设置棋子和得到棋子是使用board.setPiece()和board.getPiece()方法
     public boolean doStep(int fromX, int fromY, int toX, int toY) {
+
         return false;
     }
 
+    // 悔棋方法，从历史棋盘里面取出上一步的棋盘，复制到当前棋盘中
     public void takeBack() {
 
     }
 
+    // 判断游戏是否结束
     public boolean isFinished() {
         return false;
     }
