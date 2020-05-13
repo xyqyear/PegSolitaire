0d9651c5129ba1c6399772bc0d24c0b20c082f85
xyqyear <215908558@qq.com>
2020-05-13 19:45:36 +0800

    updated readme, added title to the window

--- src/com/xyqyear/pegsolitaire/gui/Main.java
+++ src/com/xyqyear/pegsolitaire/gui/Main.java
@@ -37,7 +37,7 @@ public class Main extends Thread {
     // Setup
     public Main() {
         // JFrame
-        frame = new JFrame();
+        frame = new JFrame("Peg Solitaire");
         frame.addWindowListener(new FrameClose());
         frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
